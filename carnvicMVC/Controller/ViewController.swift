c83311107242da50cd1f9f15171e5a69d6ed57b1
 Giritharan R <girisugu2@gmail.com>
2018-05-09 21:49:33 +0530

    testing with the vsts

--- carnvicMVC/Controller/ViewController.swift
+++ carnvicMVC/Controller/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
                 
                 let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
                 
+                
                 let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
                 if let jsonResult = jsonResult as? Dictionary<String, AnyObject>, let person = jsonResult["person"] as? [Any] {
                     // do stuff
