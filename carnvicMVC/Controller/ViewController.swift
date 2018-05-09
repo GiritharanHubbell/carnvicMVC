7054f96ccb19a71aa6c764c30ce410d1aaa60fba
 Giritharan R <girisugu2@gmail.com>
2018-05-09 21:17:55 +0530

    initial Commit

---
+++ carnvicMVC/Controller/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  carnvicMVC
+//
+//  Created by Giritharan Sugumar on 5/9/18.
+//  Copyright © 2018 giritharan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        if let path = Bundle.main.path(forResource: "UserData", ofType: "") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+                if let jsonResult = jsonResult as? Dictionary<String, AnyObject>, let person = jsonResult["person"] as? [Any] {
+                    // do stuff
+                }
+            } catch {
+                // handle error
+            }
+        }
+    }
+
+
+}
+
---
+++ carnvicMVC/Model/UserModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserModel.swift
+//  carnvicMVC
+//
+//  Created by Giritharan Sugumar on 5/9/18.
+//  Copyright © 2018 giritharan. All rights reserved.
+//
+
+import UIKit
+
+class UserModel: NSObject {
+
+}
