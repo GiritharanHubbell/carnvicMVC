eadfae8f39c6e9730345ae1047a613ab857ba0e7
 Giritharan R <girisugu2@gmail.com>
2018-05-09 21:42:07 +0530

    Added Space with new Origin

--- carnvicMVC/Controller/ViewController.swift
+++ carnvicMVC/Controller/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         if let path = Bundle.main.path(forResource: "UserData", ofType: "") {
             do {
                 
+                
                 let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
                 let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
                 if let jsonResult = jsonResult as? Dictionary<String, AnyObject>, let person = jsonResult["person"] as? [Any] {
